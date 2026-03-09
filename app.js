f13619212105214dae003c74afae0736a93724f9
ThatJack <234524594+ThatJack@users.noreply.github.com>
2026-03-08 21:17:35 -0400

    added collision and laser firing

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//load image texture
 function loadTexture(path) {
   return new Promise((resolve, reject) => {
     const img = new Image();
@@ -26,6 +27,49 @@ class GameObject {
   draw(ctx) {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
+
+  rectFromGameObject() {
+    return {
+      top: this.y,
+      left: this.x,
+      bottom: this.y + this.height,
+      right: this.x + this.width,
+    };
+  }
+}
+
+function intersectRect(r1, r2) {
+  return !(
+    r2.left > r1.right ||
+    r2.right < r1.left ||
+    r2.top > r1.bottom ||
+    r2.bottom < r1.top
+  );
+}
+
+function drawGameObjects(ctx) {
+  gameObjects.forEach((obj) => obj.draw(ctx));
+}
+
+function updateGameObjects() {
+  const enemies = gameObjects.filter((obj) => obj.type === "Enemy");
+  const lasers = gameObjects.filter((obj) => obj.type === "Laser");
+
+  lasers.forEach((laser) => {
+    enemies.forEach((enemy) => {
+      if (
+        intersectRect(laser.rectFromGameObject(), enemy.rectFromGameObject())
+      ) {
+        eventEmitter.emit(Messages.COLLISION_ENEMY_LASER, {
+          first: laser,
+          second: enemy,
+        });
+      }
+    });
+  });
+
+  //remove dead objects
+  gameObjects = gameObjects.filter((obj) => !obj.dead);
 }
 
 class Player extends GameObject {
@@ -35,13 +79,51 @@ class Player extends GameObject {
     this.height = 75;
     this.type = "Hero";
     this.speed = 5;
+    this.cooldown = 0;
+  }
+
+  fire() {
+    gameObjects.push(new Laser(this.x + 45, this.y - 10));
+    this.cooldown = 500;
+
+    let id = setInterval(() => {
+      if (this.cooldown > 0) {
+        this.cooldown -= 100;
+      } else {
+        clearInterval(id);
+      }
+    }, 200);
+  }
+
+  canFire() {
+    return this.cooldown === 0;
   }
 }
 
+//initialize player at bottom centre of screen
 function createPlayer() {
   player = new Player(canvas.width / 2 - 45, canvas.height - canvas.height / 4);
   player.img = playerImg;
   gameObjects.push(player);
+}
+
+class Laser extends GameObject {
+  constructor(x, y) {
+    super(x, y);
+    this.width = 9;
+    this.height = 33;
+    this.type = "Laser";
+    this.img = laserImg;
+
+    let id = setInterval(() => {
+      if (this.y > 0) {
+        this.y -= 15;
+      } else {
+        this.dead = true;
+        clearInterval(id);
+      }
+    }, 100);
+  }
 }
 
 class Enemy extends GameObject {
@@ -50,6 +132,7 @@ class Enemy extends GameObject {
     this.width = 98;
     this.height = 50;
     this.type = "Enemy";
+    //automatically move down until reaching bottom of screen
     const id = setInterval(() => {
       if (this.y < canvas.height - this.height) {
         this.y += 5;
@@ -61,16 +144,13 @@ class Enemy extends GameObject {
   }
 }
 
-function drawGameObjects(ctx) {
-  gameObjects.forEach((obj) => obj.draw(ctx));
-}
-
 //event handling
 class EventEmitter {
   constructor() {
     this.listeners = {};
   }
 
+  //register listeners for a message
   on(message, listener) {
     if (!this.listeners[message]) {
       this.listeners[message] = [];
@@ -78,6 +158,7 @@ class EventEmitter {
     this.listeners[message].push(listener);
   }
 
+  //inform listeners of message emission
   emit(message, payload = null) {
     if (this.listeners[message]) {
       this.listeners[message].forEach((l) => l(message, payload));
@@ -90,6 +171,9 @@ const Messages = {
   KEY_EVENT_DOWN: "KEY_EVENT_DOWN",
   KEY_EVENT_LEFT: "KEY_EVENT_LEFT",
   KEY_EVENT_RIGHT: "KEY_EVENT_RIGHT",
+  KEY_EVENT_SPACE: "KEY_EVENT_SPACE",
+  COLLISION_ENEMY_LASER: "COLLISION_ENEMY_LASER",
+  COLLISION_ENEMY_PLAYER: "COLLISION_ENEMY_PLAYER",
 };
 
 const onKeydown = function (e) {
@@ -108,6 +192,7 @@ const onKeydown = function (e) {
 };
 window.addEventListener("keydown", onKeydown);
 
+//emit events when a key is lifted
 window.addEventListener("keyup", (evt) => {
   if (evt.code === "KeyW") {
     eventEmitter.emit(Messages.KEY_EVENT_UP);
@@ -118,6 +203,9 @@ window.addEventListener("keyup", (evt) => {
     eventEmitter.emit(Messages.KEY_EVENT_LEFT);
   } else if (evt.code === "KeyD") {
     eventEmitter.emit(Messages.KEY_EVENT_RIGHT);
+  }
+  if (evt.code === "Space") {
+    eventEmitter.emit(Messages.KEY_EVENT_SPACE);
   }
 });
 
@@ -135,6 +223,7 @@ function initGame() {
   createEnemies(ctx, canvas, enemyImg);
   createPlayer();
 
+  //register player for key events
   eventEmitter.on(Messages.KEY_EVENT_UP, () => {
     player.y -= 5;
   });
@@ -147,6 +236,18 @@ function initGame() {
   eventEmitter.on(Messages.KEY_EVENT_RIGHT, () => {
     player.x += 5;
   });
+  eventEmitter.on(Messages.KEY_EVENT_SPACE, () => {
+    //fire laser
+    if (player.canFire()) {
+      player.fire();
+    }
+  });
+
+  //collision handling
+  eventEmitter.on(Messages.COLLISION_ENEMY_LASER, (_, { first, second }) => {
+    first.dead = true;
+    second.dead = true;
+  });
 }
 
 function createEnemies(ctx, canvas, enemyImg) {
@@ -156,6 +257,7 @@ function createEnemies(ctx, canvas, enemyImg) {
   const START_X = (canvas.width - FORMATION_WIDTH) / 2;
   const STOP_X = START_X + FORMATION_WIDTH;
 
+  //create pattern of enemies
   for (let x = START_X; x < STOP_X; x += ENEMY_SPACING) {
     for (let y = 0; y < 50 * 5; y += 50) {
       const enemy = new Enemy(x, y);
@@ -169,7 +271,7 @@ window.onload = async () => {
   canvas = document.getElementById("gameCanvas");
   ctx = canvas.getContext("2d");
 
-  //load textures
+  //load textures asynchronously
   enemyImg = await loadTexture("assets/enemyShip.png");
   playerImg = await loadTexture("assets/player.png");
   laserImg = await loadTexture("assets/laserRed.png");
@@ -181,5 +283,6 @@ window.onload = async () => {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     drawGameObjects(ctx);
+    updateGameObjects();
   }, 100);
 };
